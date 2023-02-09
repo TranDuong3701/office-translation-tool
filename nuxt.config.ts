5655e0525e01867419ae45535cdc4b4c25ef2820
= <tranduong3701@gmail.com>
2023-02-09 14:22:15 +0700

    :wrench: integrate element-plus UI library (#3)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  css: ["~/assets/scss/index.scss"],
+  typescript: {
+    strict: true,
+    shim: false,
+  },
+  modules: ["@element-plus/nuxt"],
+  elementPlus: {
+    icon: "ElIcon",
+  },
+});
