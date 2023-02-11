242199a8ee40399fc276769fbcbc2496bb94b55d
= <tranduong3701@gmail.com>
2023-02-11 08:56:16 +0700

    :+1: add header layout default (#2)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,43 @@
 <template>
-  <slot />
+  <div class="container-layout">
+    <header class="header-layout">
+      <img
+        class="logo"
+        src="@/assets/images/default-monochrome-black.svg"
+        alt="Office translation tool logo"
+      >
+    </header>
+    <main class="main-layout">
+      <document-translation-flow />
+      <slot />
+    </main>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.container-layout {
+  width: 100%;
+  background: var(--gray-50);
+
+  > .header-layout {
+    position: fixed;
+    width: 100%;
+    background: rgb(255, 255, 255);
+    padding: 16px 5%;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  }
+
+  > .main-layout {
+    height: 100vh;
+    width: 85%;
+    margin: auto;
+    padding-top: 15vh;
+  }
+}
+.header-layout {
+  .logo {
+    width: 50%px;
+    height: 50px;
+  }
+}
+</style>
