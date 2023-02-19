e63c8f46837e852e442af0621eb91333471dd8f1
= <tranduong3701@gmail.com>
2023-02-19 10:15:42 +0700

    :+1: add footer layout (#2)

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,9 +8,27 @@
       >
     </header>
     <main class="main-layout">
-      <document-translation-flow />
       <slot />
     </main>
+    <footer class="footer-layout">
+      <div class="left">
+        <img src="@/assets/images/isolated-monochrome-black.svg" alt="">
+      </div>
+      <div class="social-icons">
+        <Button
+          icon="pi pi-github"
+          class="p-button-outlined p-button-rounded p-button-secondary"
+        />
+        <Button
+          icon="pi pi-facebook"
+          class="p-button-outlined p-button-rounded p-button-secondary"
+        />
+        <Button
+          icon="pi pi-youtube"
+          class="p-button-outlined p-button-rounded p-button-secondary"
+        />
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -18,6 +36,7 @@
 .container-layout {
   width: 100%;
   background: var(--gray-50);
+  font-family: "Roboto";
 
   > .header-layout {
     position: fixed;
@@ -31,13 +50,36 @@
   > .main-layout {
     width: 85%;
     margin: auto;
-    padding-top: 13vh;
+    padding-top: 10vh;
+    margin-bottom: 32px;
+    min-height: 86vh;
+    overflow: hidden;
+  }
+
+  > .footer-layout {
+    border-top: 3px solid #111;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    padding: 16px 7%;
+    background: #fff;
   }
 }
 .header-layout {
   .logo {
     width: 50%px;
     height: 50px;
+  }
+}
+
+.footer-layout {
+  margin-top: 32px;
+  height: 80px;
+
+  > .social-icons {
+    display: flex;
+    align-items: center;
+    gap: 10px;
   }
 }
 </style>
